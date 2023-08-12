0d5a08efe900859615f981d971c965978e4f6651
ivy997 <49655087+ivy997@users.noreply.github.com>
2023-08-12 18:22:44 +0300

    Implement logout
    - Add new menu for the logout function

--- VirtualWaitress/app/src/main/java/com/example/virtualwaitress/MainActivity.java
+++ VirtualWaitress/app/src/main/java/com/example/virtualwaitress/MainActivity.java
@@ -2,8 +2,11 @@ package com.example.virtualwaitress;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.HorizontalScrollView;
+import android.widget.Toast;
 
 import com.example.virtualwaitress.adapters.CategoryAdapter;
 import com.example.virtualwaitress.adapters.DishAdapter;
@@ -25,6 +28,7 @@ import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import com.example.virtualwaitress.databinding.ActivityMainBinding;
+import com.google.firebase.auth.FirebaseAuth;
 
 import java.util.ArrayList;
 import java.util.Collection;
@@ -35,6 +39,7 @@ import java.util.ListIterator;
 public class MainActivity extends AppCompatActivity {
 
     private ActivityMainBinding binding;
+    private FirebaseAuth firebaseAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -46,6 +51,10 @@ public class MainActivity extends AppCompatActivity {
         BottomNavigationView navView = findViewById(R.id.nav_view);
         // Passing each menu ID as a set of Ids because each
         // menu should be considered as top level destinations.
+
+        // Initialize Firebase Authentication
+        firebaseAuth = FirebaseAuth.getInstance();
+
         AppBarConfiguration appBarConfiguration = new AppBarConfiguration.Builder(
                 R.id.navigation_home, R.id.navigation_cart, R.id.navigation_notifications)
                 .build();
@@ -58,5 +67,39 @@ public class MainActivity extends AppCompatActivity {
     // Method to refresh the activity
     public void refreshActivity() {
         recreate();
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.burger_menu, menu); // Inflate the new menu
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+
+        // Handle the items from the new menu
+        if (id == R.id.logout) {
+            // Handle the action from the new menu
+            //Toast.makeText(this, "Logout clicked", Toast.LENGTH_SHORT).show();
+            logout();
+            //return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    private void logout() {
+        // Sign out the current user
+        firebaseAuth.signOut();
+
+        // Redirect the user to the login screen (Assuming LoginActivity is the login screen)
+        Intent intent = new Intent(this, LoginActivity.class);
+        startActivity(intent);
+
+        // Finish the current activity to prevent the user from going back to it after logout
+        finish();
     }
 }
